7b29013d21687e15a1c04f44e20c338fe8d3cc3e
Jebrel Esmail <jebrelesmail@Jebrels-MacBook-Pro.local>
2024-02-15 16:38:48 -0500

    classes

---
+++ vairable.js
@@ -0,0 +1,225 @@
+// console.log("Hello World I'm a programer");
+// console.log("My Favorite Food is Fish");
+// console.log("1003 Cottman Ave Phliadelphia PA 19111");
+
+// let myFirstVariable = 1;
+// // to print the value of the variable
+
+// console.log(myFirstVariable);
+
+// let myName = "Kodoh";
+
+// console.log(myName);
+
+// let myAge = 40;
+// console.log(myAge);
+
+// let a = 5;
+// let b = 10;
+
+// let sum = a + b;
+// console.log("The sum of a and b is " + sum);
+
+// let d = 3;
+// let f = 7;
+
+// let gain = d + f;
+// console.log("The gain of d and f is " + gain);
+
+// let difference = a - b;
+// console.log("The difference of a and b is  " + difference);
+
+// let cost = d - f;
+// console.log("The cost of d and f is " + cost);
+
+// console.log("My Name is Jebrel Ali");
+// console.log("My profissional is am a student");
+// console.log("My country is United States");
+
+// let Myname = "Jebreel";
+// console.log(Myname);
+
+// let MyCountry = "United States";
+// console.log(MyCountry);
+
+
+// let NumberofChildren = "N";
+// console.log(NumberofChildren);
+
+// let PartnerName = "Z";
+// console.log(PartnerName);
+
+// let GeoraphicLocation = "Y";
+// console.log(GeoraphicLocation);
+
+// let JobTitle = "X";
+// console.log(JobTitle);
+
+// let Num1 = 150;
+// let Num2 = 10;
+
+// let Product = Num1 + Num2;
+// console.log("The Product of Num1 and Num2" + Product);
+
+// let Division = Num1 / Num2;
+// console.log("The Division of Num1 / Num2 " + Division);
+
+// let Difference = Num1 - Num2;
+// console.log("The Differrnce of Num1 and Num2 " + Difference);
+
+// let Additional = Num1 + Num2;
+// console.log("The Additional of Num1 and Num2 " + Additional);
+
+
+// let a = 5;
+// let b = 4;
+
+// console.log(a == b);
+// console.log(a != b);
+// console.log(a > b);
+// console.log(a < b);
+// console.log(a >= b);
+// console.log(a <= b);
+
+// let FirstName = "Jebrel";
+// let LastName = "Ali";
+
+// console.log(FirstName == LastName);
+// console.log(FirstName != LastName);
+// console.log(FirstName > LastName);
+// console.log(FirstName < LastName);
+// console.log(FirstName >= LastName);
+// console.log(FirstName <= LastName);
+
+// let a = 2.13;
+// let b = 5.34;
+
+// console.log(a == b);
+// console.log(a != b);
+// console.log(a > b);
+// console.log(a < b);
+// console.log(a >= b);
+// console.log(a <= b);
+
+// let a = 5;
+// a++;
+// console.log(a);
+
+// let d = 0.2;
+// d++;
+// console.log(d);
+
+// let f = 0.1;
+// f--;
+// console.log(f);
+
+// let c = 13.3;
+// c--;
+// console.log(c);
+
+// let a = 5;
+// a+=2;
+// console.log(a);
+
+// let q =10;
+// q+=2;
+// console.log(q);
+
+// let p = -11.5;
+// p+=2;
+// console.log(p);
+
+// let a = 7;
+// a+=20;
+// console.log(a);
+
+// let x = 9;
+// x-=2;
+// console.log(x);
+
+// let s = -2;
+// s-=2;
+// console.log(s);
+
+// let f = 2.13;
+// f-=4;
+// console.log(f);
+
+// let z = 14.7;
+// z-=7;
+// console.log(z);
+
+// let b = 5;
+// b*=2;
+// console.log(b);
+
+// let n = 11.4;
+// n*=3;
+// console.log(n); 
+
+// let m = -6;
+// m*=3;
+// console.log(m);
+
+// let a = 10;
+// a/=3;
+// console.log(a);
+
+// let g = -20;
+// g/=4;
+// console.log(g);
+
+// let e = 35;
+// e/=3.4;
+// console.log(e);
+
+
+// let sun = 20;
+// sun+=10;
+// console.log(sun);
+
+// let sub = 20;
+// sub-=10;
+// console.log(sub);
+
+// let suz = 20;
+// suz*=10;
+// console.log(suz);
+
+// console.log((3<5) && (2<4));
+// console.log((3>5) && (2>4));
+// console.log((3<5) && (2>4));
+// console.log((3>5) && (2>4));
+
+
+// console.log((5.5<7.5) && (5.5<7.5));
+// console.log((5.5>7.5) && (5.5>7.5));
+// console.log((5.5<7.5) && (5.5>7.5));
+// console.log((5.5>7.5) && (5.5>7.5));
+
+
+// console.log(("Jebrel"<"Nishtha") && ("Jebrel"<"Nishtha"));
+// console.log(("Jebrel">"Nishtha") && ("Jebrel">"Nishtha"));
+// console.log(("Jebrel"<"Nishtha") && ("Jebrel">"Nishtha"));
+// console.log(("Jebrel">"Nishtha") && ("Jebrel">"Nishtha"));
+
+// console.log(("Ebrahim"<"Esmail") && ("Ebrahim"<"Esmail"));
+// console.log(("Ebrahim">"Esmail") && ("Ebrahim">"Esmail"));
+// console.log(("Ebrahim"<"Esmail") && ("Ebrahim">"Esmail"));
+// console.log(("Ebrahim">"Esmail") && ("Ebrahim">"Esmail"));
+
+
+let a = 0;
+console.log(a);
+
+function coolFunction() {
+    let b = 2;
+    console.log(`a = ${a}, b = ${b}.`);
+    return a + b;
+}
+
+a += 1;
+console.log(a);
+
+coolFunction();
+console.log(b);
